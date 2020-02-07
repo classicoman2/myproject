1bd8ff80425a74680698819ad830f9285476c788
Classicoman2 <aamengual@iesfbmoll.org>
2020-02-07 20:46:10 +0100

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Estam a l'any" v-bind:any="year" />
   </div>
 </template>
 
@@ -12,17 +12,16 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+data: function() { 
+   return {
+     //Aqui les meves data
+     year: 2020
+   }
+ }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
